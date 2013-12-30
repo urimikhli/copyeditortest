c336b550a467ca3023ce0f5d4d601897c522b419
Uri Mikhli <urimikhli@gmail.com>
2013-12-29 19:09:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Copyeditortest::Application.config.secret_key_base = '4d5497b177ddc0871fc0b3da96e140483a83cee002cb54257a2b9d12fcdf0bfb12524ac9901323245fc776c0d0416461b21891b2dbb078484fe061524a9c6b83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Copyeditortest::Application.config.session_store :cookie_store, key: '_copyeditortest_session'
