c29630bc7f818da91b0ccb9a6558f014f599d341
Uri Mikhli <urimikhli@gmail.com>
2014-02-08 22:09:24 -0500

    Create Home Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Copyeditortest::Application.routes.draw do
+  get "home/about"
+  get "home/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
